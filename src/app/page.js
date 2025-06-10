74fffb7e65e417098e93d35e0d997abb4e2be391
zyadahmedd <zyada861@gmail.com>
2025-06-10 03:53:10 +0300

    first-Branch

---
+++ src/app/page.js
@@ -0,0 +1,16 @@
+import "../../public/css/style.css";
+import "../../public/css/mediaQuery.css";
+
+import Nav from "../component/Naviga";
+import Footer from "../component/Footer";
+export default function Home() {
+  return (
+    <>
+    <Nav />
+    <main>
+      maaaaaaaaain
+    </main>
+    <Footer/>
+    </>
+  );
+}
---
+++ src/component/Naviga.js
@@ -0,0 +1,33 @@
+
+export default function Naviga() {
+  return (
+    <div>
+            <nav>
+        <h1>My Portfolio</h1>
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/about">About</a>
+          </li>
+          <li>
+            <a href="/contact">Contact</a>
+          </li>
+          <li>
+            <a href="/blog">Blog</a>
+          </li>
+          <li>
+            <a href="/services">Services</a>
+          </li>
+        </ul>
+        <div className="menu-icon">
+          <span className="bar"></span>
+          <span className="bar"></span>
+          <span className="bar"></span>
+        </div>
+      </nav>
+
+    </div>
+  )
+}
